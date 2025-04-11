daa0d80174812a0b490b42c4450e1e684600878e
Josby1 <josby1@hotmail.com>
2025-04-11 18:55:59 +1000

    added string split and word count

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("greetings boots")
+def main():
+	file_path = "books/frankenstein.txt"
+	file_contents = get_book_text(file_path)
+	num_words = number_words(file_contents)
+	print (f"{num_words} words found in the document")
+	
+	
+	
+
+
+def get_book_text(f_path):
+	with open(f_path) as f:
+		f_contents = f.read()
+	return(f_contents)
+
+def number_words(full_text):
+	split_text = full_text.split(' ')
+	return len(split_text)
+
+
+	
+
+
+main()
+
